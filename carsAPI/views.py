e6b203b8308c9ee140979723aa90d64ffae28d4b
Dmitry Shishkov <dim457676@gmail.com>
2024-01-06 15:37:46 +0300

    small corrections

--- carsAPI/views.py
+++ carsAPI/views.py
@@ -3,6 +3,7 @@ from .serializers import CarsSerializer
 from rest_framework import viewsets
 from .permissions import CustomPermission
 from rest_framework import filters
+from django.http import JsonResponse
 
 class CarsViewSet(viewsets.ModelViewSet):
     queryset = Cars.objects.all()
@@ -10,3 +11,17 @@ class CarsViewSet(viewsets.ModelViewSet):
     permission_classes = (CustomPermission,)
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     search_fields = ['brand', 'model']
+
+def checkAuth(request):
+    if request.user.is_authenticated:
+        data = {
+            #"params": request.headers.get(),
+            "auth": "ok",
+            "username": request.user.username
+        }
+    else:
+        data = {
+            "params": request.headers.get('Authorization'),
+            "auth": "invalid"
+        }
+    return JsonResponse(data, status=200)
