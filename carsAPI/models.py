627fcfb2a19350be3822e928bd7664d130e199ed
admin <dim457676@gmail.com>
2024-01-19 18:35:01 +0300

    added autogeneration token for auth

--- carsAPI/models.py
+++ carsAPI/models.py
@@ -1,6 +1,9 @@
 import os
-
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 # Create your models here.
 class Cars(models.Model):
@@ -23,3 +26,8 @@ class Cars(models.Model):
         upload_to='./static/'
     )
 
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
